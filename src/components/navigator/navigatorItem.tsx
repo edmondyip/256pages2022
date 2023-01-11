7b3add8da7c4472ef13ed1595f576e5991455ce3
edmondyip <neta.edmond@gmail.com>
2023-01-11 02:09:28 +0000

    update nav font

--- src/components/navigator/navigatorItem.tsx
+++ src/components/navigator/navigatorItem.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { navItem } from "types/layout";
 import styles from "./navigator.module.scss";
-import { outfit } from "styles/fonts";
+import { firaCode } from "styles/fonts";
 
 interface pageProps {
 	item: navItem;
@@ -11,7 +11,7 @@ interface pageProps {
 export const PageNavigatorItem = ({ item }: pageProps) => {
 	return (
 		<li className={styles.items}>
-			<Link className={outfit.className} href={item.path}>
+			<Link className={firaCode.className} href={item.path}>
 				{item.label}
 			</Link>
 		</li>
