a5fd427099db8964c4debcd414fcdc9290dd7aee
edmondyip <neta.edmond@gmail.com>
2023-01-11 02:08:36 +0000

    add copyright

---
+++ src/components/copyright/index.tsx
@@ -0,0 +1,7 @@
+import { outfit } from "styles/fonts";
+
+const Copyright = () => {
+	return <div className={`absolute bottom-2 right-2 text-xs ${outfit.className}`}>&copy; 256pages</div>
+};
+
+export default Copyright;
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -4,6 +4,8 @@ import { useRouter } from "next/router";
 import { PageNavigator } from "components/navigator";
 import { useGetLayoutQuery } from "store/feature/layout/layoutApi";
 import Logo from "components/logo";
+import Copyright from "components/copyright";
+import { firaCode, bigShouldersDisplay } from "styles/fonts"
 
 const ThemeSwitch = dynamic(() => import("components/themeSwitch"), {
 	ssr: false,
@@ -20,13 +22,16 @@ const Layout = ({ children }: IProps) => {
 
 	return (
 		<>
-			<header className="container mx-auto flex py-2">
+			<header className="container mx-auto grid grid-cols-[min-content,1fr,min-content] items-center p-2">
 				{isLoading ? <>loading</> : null}
 				<Logo />
 				{data ? <PageNavigator nav={data.nav} /> : null}
 				<ThemeSwitch />
 			</header>
-			<main className={pathname === "/" ? "index" : "container mx-auto"}>{children}</main>
+			<main className={`${pathname === "/" ? "index" : "container mx-auto p-2"} ${bigShouldersDisplay.variable} ${firaCode.variable}`}>{children}</main>
+			<footer>
+				<Copyright />
+			</footer>
 		</>
 	);
 };
