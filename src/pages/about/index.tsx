ca0e83aecc1d00f06484c19c9ce174b04ceadfd2
edmondyip <neta.edmond@gmail.com>
2023-01-17 09:39:55 +0000

    feat: icon for about

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -18,7 +18,7 @@ export const getStaticProps: GetStaticProps = async () => {
 };
 
 interface PageProps {
-	content: pageContent;
+	content: Record<string, pageContent>;
 	metaDescription: metaDescription;
 }
 
@@ -26,9 +26,119 @@ const AboutPage = ({ content, metaDescription }: PageProps) => {
 	return (
 		<>
 			<MetaHead meta={metaDescription} />
-				<BackgroundTitle title={content.title} />
-			<section>
-				<p>{content.content}</p>
+			<BackgroundTitle title="about" />
+			<section className="before:content-01">
+				<h2>
+					<span className="text-blue-600">whoAmI</span>() &#123;
+				</h2>
+				<p>
+					<span className="text-pink-400">return</span> "I am{" "}
+					<span className="text-orange-300">Edmond Yip</span>. A{" "}
+					<span className="text-orange-300">Frontend Engineer</span> 👨‍💻 base in{" "}
+					<span className="text-orange-300">Bristol 🇬🇧</span>. Working with{" "}
+					<span className="text-orange-300">Typescript, React and Vue</span>."
+				</p>
+				<div className="text-yellow-400 text-size-3xl">&#125; ;</div>
+			</section>
+			<section className="before:content-02">
+				<h2>
+					<span className="text-blue-600">geographicalLocation</span>() &#123;
+				</h2>
+				<p>
+					<span className="text-pink-400">return</span> "Born and educated in{" "}
+					<span className="text-orange-300">Hong Kong 🇭🇰</span>. Relocated to
+					the <span className="text-orange-300">United Kingdom 🇬🇧</span> in
+					2022."
+				</p>
+				<div className="text-yellow-400 text-size-3xl">&#125; ;</div>
+			</section>
+			<section className="before:content-03">
+				<h2>
+					<span className="text-blue-600">careerPath</span>() &#123;
+				</h2>
+
+				<p>
+					<span className="text-pink-400">return</span> "I change my career from
+					a <span className="text-orange-300">Web Designer 🎨</span> to{" "}
+					<span className="text-orange-300">Frontend Engineer 💻</span> at
+					2017."
+				</p>
+				<div className="text-yellow-400 text-size-3xl">&#125; ;</div>
+			</section>
+			<section className="before:content-04">
+				<h2>
+					<span className="text-blue-600">techSkills</span>() &#123;
+				</h2>
+				<p>
+					<span className="text-blue-600">const</span>{" "}
+					<span className="text-orange-300">css</span> = "Using{" "}
+					<span className="text-orange-300">
+						SASS <img width={22} src="/img/sass.svg" />
+					</span>{" "}
+					to{" "}
+					<span className="text-orange-300">
+						TailwindCSS <img width={22} src="/img/tailwindcss.svg" />
+					</span>
+					."
+				</p>
+				<p>
+					<span className="text-blue-600">const</span>{" "}
+					<span className="text-orange-300">framework</span> = "Learning with{" "}
+					<span className="text-orange-300">
+						Vue <img width={22} src="/img/vue.svg" /> and Nuxt{" "}
+						<img width={22} src="/img/nuxt.svg" />
+					</span>{" "}
+					to Working with{" "}
+					<span className="text-orange-300">
+						React <img width={22} src="/img/react.svg" /> and Next
+					</span>
+					."
+				</p>
+
+				<p>
+					<span className="text-blue-600">const</span>{" "}
+					<span className="text-orange-300">language</span> = "From using{" "}
+					<span className="text-orange-300">
+						Javascript <img width={22} src="/img/javascript.svg" />
+					</span>{" "}
+					to using{" "}
+					<span className="text-orange-300">
+						Typescript <img width={22} src="/img/typescript.svg" />
+					</span>
+					."
+				</p>
+
+				<p>
+					<span className="text-blue-600">const</span>{" "}
+					<span className="text-orange-300">others</span> = &#91;
+					<span className="text-orange-300">"Git"</span>,
+					<span className="text-orange-300">"Node"</span>,
+					<span className="text-orange-300">"Express"</span>,
+					<span className="text-orange-300">"Nginx"</span>,
+					<span className="text-orange-300">"Docker"</span>,
+					<span className="text-orange-300">"Webpack"</span>,
+					<span className="text-orange-300">"Threejs"</span>,
+					<span className="text-orange-300">"Jest"</span>
+					&#93;
+				</p>
+				<p>
+					<span className="text-pink-400">return</span> &#123; css, framework,
+					language, others &#125;
+				</p>
+				<div className="text-yellow-400 text-size-3xl">&#125; ;</div>
+			</section>
+			<section className="before:content-05">
+				<h2>
+					<span className="text-blue-600">interest</span>() &#123;
+				</h2>
+				<p>
+					<span className="text-pink-400">return</span> "
+					<span className="text-orange-300">Playing Video Game 🎮 </span> ,{" "}
+					<span className="text-orange-300">Taking Photo 📷 </span> &{" "}
+					<span className="text-orange-300">I ❤️ My Family 👨‍👩‍👧‍👧 </span>
+					."
+				</p>
+				<div className="text-yellow-400 text-size-3xl">&#125; ;</div>
 			</section>
 		</>
 	);
--- src/pages/api/about.ts
+++ src/pages/api/about.ts
@@ -2,26 +2,35 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { metaDescription, pageContent } from "types/layout";
 
 interface aboutProps {
-	content: pageContent;
+	content: Record<string, pageContent>;
 	metaDescription: metaDescription;
-	history: pageContent;
 }
 
 export default (req: NextApiRequest, res: NextApiResponse<aboutProps>) => {
 	res.status(200).json({
-		content: {
-			title: "about",
-			content: "Hi, I am Edmond Yip, a Frontend Engineer 👨‍💻 base in UK 🇬🇧. Working with Typescript, React and Vue.",
-		},
 		metaDescription: {
 			pageTitle: "about - 256pages",
 			pageDescription:
 				"Hi, i am a designer from Hong Kong. As my position is a full stack designer, that's mean i can design from graphic to user experience, also the front-end programming.",
 			canonicalUrl: "/about",
 		},
-		history: {
-			title: "about 256pages",
-			content: "",
+		content: {
+			"about": {
+				title: "about",
+				content: "Hi, I am Edmond Yip, a Frontend Engineer 👨‍💻 base in Bristol 🇬🇧. Working with Typescript, React and Vue.",
+			},
+			"location": {
+				title: "location",
+				content: "I living from Hong Kong 🇭🇰 to United Kingdom 🇬🇧"
+			},
+			"position": {
+				title: "position",
+				content: "I working from Design 🎨 to Develop 💻"
+			},
+			"framework": {
+				title: "framework",
+				content: "I learning from Vue to React"
+			}
 		},
 	});
 };
