beb5cc7d44c457d92c0761a1e5350cabc60a7107
edmondyip <neta.edmond@gmail.com>
2023-01-17 09:41:34 +0000

    fix: variable font weight

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,25 +1,30 @@
-import { Big_Shoulders_Display, Outfit, Fira_Code, Oswald } from "@next/font/google";
+import {
+	Big_Shoulders_Display,
+	Outfit,
+	Fira_Code,
+	Oswald,
+} from "@next/font/google";
 
 export const bigShouldersDisplay = Big_Shoulders_Display({
-  subsets: ["latin"],
-	weight: "500",
-	variable: "--bigShouldsDisplay-font",
+	subsets: ["latin"],
+	weight: "variable",
+	variable: "--bigShouldersDisplay-font",
 });
 
 export const outfit = Outfit({
 	subsets: ["latin"],
-	weight: "400",
-	variable: "--outfit-font"
-})
+	weight: "variable",
+	variable: "--outfit-font",
+});
 
 export const firaCode = Fira_Code({
 	subsets: ["latin"],
-	weight: "300",
-	variable: "--firaCode-font"
-})
+	weight: "variable",
+	variable: "--firaCode-font",
+});
 
 export const oswald = Oswald({
 	subsets: ["latin"],
-	weight: "700",
-	variable: "--oswald-font"
-})
+	weight: "variable",
+	variable: "--oswald-font",
+});
